760e336e9addcc22e297a20df9ea4c1ed9851c52
arthurmloureiro <arthurmloureiro@gmail.com>
2014-01-23 15:25:46 -0200

    Algo errado na fft

--- fft_teste1.py
+++ fft_teste1.py
@@ -7,15 +7,18 @@
 """
 import numpy as np
 import pylab as pl
-L=100
+N=100
+tamanho = 40000
 #x=np.linspace(0.0,np.pi*2, L)
-x=np.linspace(-10.,10., L)
+#x=np.linspace(-10.,10., L)
+x = 1./tamanho * np.arange(N)
+froc = (tamanho/N)*np.arange(N)
 #x=np.random.random(L)*2*np.pi
-freq1 = 2.0
-freq2 = 332.45
+freq1 = 50.0
+freq2 = 300.45
 A_0 = 2.0
-#MM=A_0*np.cos(freq1*x)+np.cos(freq2*x)		#cosseno
-M=A_0*np.exp(-(freq1*x**2)/2)		#gaussiana
+M= A_0*np.sin(2.*np.pi*freq1*x) + np.sin(2.*np.pi*freq2*x)		#cosseno
+#M=A_0*np.exp(-(freq1*x**2)/2)			#gaussiana
 #M = A_0*np.exp(1j*freq1*x)			#exponencial normal
 
 four = np.fft.fft(M)
@@ -23,10 +26,10 @@ freq_ = np.fft.fftfreq(x.shape[-1])
 
 fig = pl.figure()
 fig.subplots_adjust(hspace=0.50)
-ax1 = fig.add_subplot(311)					#faz subplots, 2-> graf na horiz, 1 -> na vertical, 1-> numero do grafico
+ax1 = fig.add_subplot(311)					#faz subplots, 3-> graf na horiz, 1 -> na vertical, 1-> numero do grafico
 ax2 = fig.add_subplot(312)
 ax3 = fig.add_subplot(313)
-ax2.plot(four.real)
+ax2.plot(froc,four.real)
 #fig.title('transformada de $10*cos(27*x)*cos(47*x)$')
 ax1.set_title('$f(x) =' + str(A_0) + ' *cos(' + str(freq1)+ '*x) + cos('+str(freq2)+'*x)$')
 ax3.set_title(r'$|\bar{F(k)}|^2$')
