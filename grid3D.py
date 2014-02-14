98daa376b2b2d65ff82e9187fece90166ed234e8
arthurmloureiro <arthurmloureiro@gmail.com>
2014-02-14 13:31:09 -0200

    Qualquer numero e tamanho de celula em todas direções

--- grid3D.py
+++ grid3D.py
@@ -19,30 +19,32 @@ class grid3d:
 	'''
 	a entrada é o tamanho dos vetores k_x, k_y, k_z
 	'''
-	def __init__(self,m,n,l,L_):
-		self.size_x = m
-		self.size_y = n
-		self.size_z = l
-		self.L = L_
+	def __init__(self,n_x,n_y,n_z,L_x,L_y,L_z):
+		self.size_x = n_x
+		self.size_y = n_y
+		self.size_z = n_z
+		self.Lx = L_x
+		self.Ly = L_y
+		self.Lz = L_z
 		
-		kx0 = (2*np.pi)/L_				# k0 tem que ser este valor para que |k| < k_max
-		ky0 = (2*np.pi)/L_
-		kz0 = (2*np.pi)/L_
+		kx0 = (2*np.pi)/L_x				# k0 tem que ser este valor para que |k| < k_max
+		ky0 = (2*np.pi)/L_y
+		kz0 = (2*np.pi)/L_z
 		
 		
-		prime_x=np.arange(1,(m/2+1),1)*kx0		# tem que ser até m/2 +1 por causa da estrutura do np.arange
+		prime_x=np.arange(1,(n_x/2+1),1)*kx0		# tem que ser até m/2 +1 por causa da estrutura do np.arange
 		invert_prime_x = -prime_x[::-1]			
 		prime_x = np.insert(prime_x, 0,0)		
 		self.k_x = np.append(prime_x,invert_prime_x)		
 
 
-		prime_y=np.arange(1,(n/2+1),1)*ky0		
+		prime_y=np.arange(1,(n_y/2+1),1)*ky0		
 		invert_prime_y = -prime_y[::-1]			
 		prime_y = np.insert(prime_y, 0,0)		
 		self.k_y = np.append(prime_y,invert_prime_y)		
 
 
-		prime_z=np.arange(1,(l/2+1),1)*kz0		#
+		prime_z=np.arange(1,(n_z/2+1),1)*kz0		#
 		invert_prime_z = -prime_z[::-1]			#inverte a ordem de prime e a deixa negativa
 		prime_z = np.insert(prime_z, 0,0)		#adiciona o valor zero na posição 0
 		self.k_z = np.append(prime_z,invert_prime_z)	#junta todos os vetores
