2b3b1a5f23cc79031cb3a5036a6f22dbd162cf70
arthurmloureiro <arthurmloureiro@gmail.com>
2014-02-24 14:46:16 -0300

    Modificado pelo Raul e por mim: faltava normalizar por sqrt(2) (???)

--- pk.py
+++ pk.py
@@ -11,6 +11,7 @@
 		12/12/2013
 """
 import numpy as np
+import sys
 import pylab as pl
 import grid3D as gr
 from mpl_toolkits.mplot3d.axes3d import Axes3D
@@ -28,24 +29,36 @@ k_r , P_k = np.loadtxt('fid_matterpower.dat', unpack=True)		#pega o P(k) do Raul
 k_r = np.insert(k_r,0,0.)						#insere P(k=0) = 0 antes de interpolar
 P_k = np.insert(P_k,0,0.)						#mesma coisa que acima
 
-
 k = gr.grid3d(N_x,N_y,N_z,l_x,l_y,l_z)					#cria o grid com qualquer tamanho em 3D
 volume = l_x*l_y*l_z							
+
 Pk = interpolate.InterpolatedUnivariateSpline(k_r,P_k)			#interpola os dados do CAMB
 
 p_matrix =np.asarray([[[ Pk(k.matrix[i][j][n]) for i in range(len(k.k_x))] for j in range(len(k.k_y))] for n in range(len(k.k_z))])
 
+
 def A_k(P_):
-	return np.random.normal(0,np.sqrt(2.*P_*volume))		#distribuicao gaussiana media no zero E DESVIO SQRT(2*P_k)
+	return np.random.normal(0,np.sqrt(P_*volume))		#distribuicao gaussiana media no zero E DESVIO SQRT(2*P_k)
 									#incluído volume para fechar as unidades da maneira certa
 def phi_k(P_): 
 #	return (np.random.random(len(p_matrix)))*2.*np.pi - np.pi	#distr. homog. de -pi a +pi
 	return (np.random.random(len(P_)))*2.*np.pi			#segundo Padmanabhan pg 191 ===> É A MESMA COISA
+
 def delta_k(P_):							
 	return A_k(P_)*np.exp(1j*phi_k(P_))				#contraste de densidade em k
 
+
+
 ############################# FFT #######################################
-delta_x = ((delta_k(p_matrix).size)/volume)*np.fft.ifftn(delta_k(p_matrix)).real
+delta_x = ((delta_k(p_matrix).size)/volume)*np.fft.ifftn(delta_k(p_matrix))
+
+print "Campo complexo: " + str(delta_x[1,2,3])  
+
+delta_xr = delta_x.real
+delta_xi = delta_x.imag
+
+print "Parte real: " + str(delta_xr[1,2,3])  + " e parte imaginaria: " + str(delta_xi[1,2,3])
+
 #########################################################################
 
 #k_max_cal = np.sqrt(np.power(np.max(k.k_x),-2)+np.power(np.max(k.k_y),-2)+np.power(np.max(k.k_z),-2))
@@ -60,9 +73,13 @@ print "Lado da celula: " + str(np.power(volume,1./3)/len(delta_x)) + " Mpc"	#par
 #print "raio da celula (dados iniciais)    : " + str( np.sqrt(d_x**2 + d_y**2 + d_z**2) )
 #print "raio da celula (usando a matriz K) : " + str( np.pi/np.max(k.matrix) ) + " Mpc"
 #print "raio da celula (usando a definição): " + str( np.pi/k_max_cal) + " Mpc"
-print "<delta_r^2> = " + str(np.std(delta_x))				#deve ser aprox. 0.9
+print "<delta_rr^2> = " + str(np.std(delta_xr))				#deve ser aprox. 0.9
+print "<delta_ri^2> = " + str(np.std(delta_xi))				#deve ser aprox. 0.9
+
 #print "k_max calculado: " + str(k_max_cal) 
 print "k_max da matriz de k: " + str(np.max(k.matrix))
+sys.exit(-1)
+
 print "################################################################"
 k.plot	
 pl.colorbar()								#plota a matriz dos k's
